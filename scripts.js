cca5c0cc0ca04fa71e9233a88121809963601f59
theylorantunes <theylorantunes@gmail.com>
2026-02-24 22:15:17 -0300

    ex 3

--- scripts.js
+++ scripts.js
@@ -1,50 +1,38 @@
 const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
-const clearStorageBtn = document.getElementById('clearStorageBtn');
+
 
 let tasks = JSON.parse(localStorage.getItem('minhasTarefas')) || [];
 
 function renderTasks() {
-    taskList.innerHTML = ''; 
-    tasks.forEach((task, index) => {
+    taskList.innerHTML = '';
+    tasks.forEach((task) => {
         const li = document.createElement('li');
         li.textContent = task;
-        li.className = 'task-item';
         taskList.appendChild(li);
     });
 }
 
 
-function saveToLocalStorage() {
-
-    localStorage.setItem('minhasTarefas', JSON.stringify(tasks));
-}
+taskInput.addEventListener('input', (e) => {
+    sessionStorage.setItem('rascunho', e.target.value);
+});
 
 
 addTaskBtn.addEventListener('click', () => {
     const text = taskInput.value.trim();
     if (text !== "") {
         tasks.push(text);
-        saveToLocalStorage();
+  
+        localStorage.setItem('minhasTarefas', JSON.stringify(tasks));
+        
         renderTasks();
+        
+
         taskInput.value = "";
-        sessionStorage.removeItem('rascunho'); 
+        sessionStorage.removeItem('rascunho');
     }
-});
-
-
-clearStorageBtn.addEventListener('click', () => {
-    if (confirm("Deseja apagar todas as tarefas?")) {
-        tasks = []; 
-        localStorage.removeItem('minhasTarefas'); 
-        renderTasks(); 
-    }
-});
-
-
-taskInput.addEventListener('input', (e) => {
-    sessionStorage.setItem('rascunho', e.target.value);
 });
 
 
